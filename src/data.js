88563f23082879fbf47516516bc521991b28fd5a
Evelynbotia <evelynbotia07@gmail.com>
2021-03-18 19:47:53 -0500

    Primera prueba acceder a la data

--- src/data.js
+++ src/data.js
@@ -4,6 +4,13 @@ export const example = () => {
   return 'example';
 };
 
+export const filterName = (data) => {
+  const results = data.map(character => character.name);
+
+  console.log(results);
+  return results;
+}
+
 export const anotherExample = () => {
   return 'OMG';
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+import data from './data/rickandmorty/rickandmorty.js';
+
+//console.log(example, data);
+
+
+const botonSpecies = document.getElementsByClassName("speciesMenu");
+for(let i = 0; i< botonSpecies.length; i ++){
+    botonSpecies[i].addEventListener("click", () => {
+      const birthPlace = document.getElementById("birthPlace");
+      birthPlace.style.display = "block";
+    });
+    
+    console.log(birthPlace);
+    
+}
+
+
+
+
+// let characters = data.results;
+
+// console.log(characters);
+
+// document.getElementById("parrafo").appendChild = characters;
+// // document.getElementById("characters").addEventListener("onsubmit",myFunction);
+
+// function myFunction() {
+//   var btn = document.createElement("BUTTON");
+//   btn.innerHTML = "CLICK ME";
+//   document.body.appendChild(btn);
+// }
