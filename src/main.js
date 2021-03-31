643c6979379eb6293fe00730aeb29e9144005856
Evelynbotia <evelynbotia07@gmail.com>
2021-03-30 21:16:32 -0500

    Tarjetas con estilo unificado

--- src/main.js
+++ src/main.js
@@ -16,34 +16,42 @@ console.log(data.results);
 
 const todosLosPersonajes= data.results;
 
-//document.getElementById("estructuraPrueba").style.display ="none";
+document.getElementById("fichaPersonajes").style.display ="none";
 
 for (let i=0;i<todosLosPersonajes.length;i++){
 
   let contenedorpersonajes= document.getElementById("personajes");// creo las variables la ubicacion a remplazar
-  let nuevoElemento = document.createElement("div");// le estoy diciendo que debe crear un nuevo div
+  let nuevoDiv = document.createElement("div");// le estoy diciendo que debe crear un nuevo div
   let nuevaImagen = document.createElement("img");// le estoy diciendo que debe crear una imagen
-  let nuevoNombre = document.createElement("h1")// le estoy diciendo que debe crear un parrafo
-  let nuevoParrafo = document.createElement("p");
-  nuevoElemento.className="card";//s ele asigna la class card al div nuevo elemento
- 
+  let nuevoDivImg = document.createElement ("div");
+  let nuevoNombre = document.createElement("h1");// le estoy diciendo que debe crear un parrafo
+  let nuevoParrafo = document.createElement("h2");
+  let nuevoDivContent = document.createElement("div")
+  nuevoDiv.className="card";//s ele asigna la class card al div nuevo elemento
+  nuevoDivImg.className="imgBx";
+  nuevoDivContent.className ="content";
        
   nuevaImagen.src=todosLosPersonajes[i].image;
-  nuevoNombre.innerHTML=todosLosPersonajes[i].name;
+  nuevoNombre.innerHTML= todosLosPersonajes[i].name;
+  nuevoParrafo.innerHTML = "Species: " +  todosLosPersonajes[i].species + " <br>Gender: " + todosLosPersonajes[i].gender + "<br>Status: " + todosLosPersonajes[i].status + "<br> Origin: " + todosLosPersonajes[i].origin.name + "<br> Type: " + todosLosPersonajes[i].type;
+  
+
+  nuevoDivImg.appendChild(nuevaImagen);//sonre el nuevo elemento se va acrear  la imagen 
+  nuevoDivContent.appendChild(nuevoNombre);
+  nuevoDivContent.appendChild(nuevoParrafo);
+  nuevoDiv.appendChild(nuevoDivImg);
+  nuevoDiv.appendChild(nuevoDivContent);
+  contenedorpersonajes.appendChild(nuevoDiv);//crear el div que contiene la imagen y que contiene el nombre copiando el formato de card
    
-  nuevoElemento.appendChild(nuevoParrafo);
-  nuevoElemento.appendChild(nuevaImagen);//sonre el nuevo elemento se va acrear  la imagen 
-  nuevoElemento.appendChild(nuevoNombre);
-  contenedorpersonajes.appendChild(nuevoElemento);//crear el div que contiene la imagen y que contiene el nombre copiando el formato de card
-   
-  nuevoElemento.id= todosLosPersonajes[i].id
+  nuevoDiv.id= todosLosPersonajes[i].id
 
 
-  nuevoElemento.addEventListener('click', mostrarInfo);
+  // nuevoElemento.addEventListener('click', mostrarInfo);
   
   
 }
-document.getElementById("specie").style.display ="none";
+
+// document.getElementById("specie").style.display ="none";
 function mostrarInfo(event){
   let id =event.currentTarget.id;
   let infoObjectById = todosLosPersonajes.find(elemento => elemento.id==id)
