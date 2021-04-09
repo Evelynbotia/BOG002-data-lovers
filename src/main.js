2daa54b948c9b547bfc71a2fd695d5a072e1c238
JessiJoya <jessica.joyaq@gmail.com>
2021-04-08 19:34:21 -0500

    edicion antes de crear botones con create elemete

--- src/main.js
+++ src/main.js
@@ -15,107 +15,94 @@ import data from './data/rickandmorty/rickandmorty.js';
 // console.log(data.results);
 
 const todosLosPersonajes = data.results;
+console.log(todosLosPersonajes);
 
 
 showCards(todosLosPersonajes);
 
 function showCards(resultadoPersonajes) {
-  let contenedorpersonajes = document.getElementById("personajes");// creo las variables la ubicacion a remplazar
+   const contenedorpersonajes = document.getElementById("personajes");// creo las variables la ubicacion a remplazar
   contenedorpersonajes.innerHTML = "";
   for (let i = 0; i < resultadoPersonajes.length; i++) {
-
-
-    let nuevoDiv = document.createElement("div");// le estoy diciendo que debe crear un nuevo div
-    let nuevaImagen = document.createElement("img");// le estoy diciendo que debe crear una imagen
-    let nuevoDivImg = document.createElement("div");
-    let nuevoNombre = document.createElement("h1");// le estoy diciendo que debe crear un parrafo
-    let nuevoParrafo = document.createElement("h2");
-    let nuevoDivContent = document.createElement("div")
-    nuevoDiv.className = "card";//s ele asigna la class card al div nuevo elemento
+    const nuevoDiv = document.createElement("div");// le estoy diciendo que debe crear un nuevo div
+    const nuevaImagen = document.createElement("img");// le estoy diciendo que debe crear una imagen
+    const nuevoDivImg = document.createElement("div");
+    const nuevoNombre = document.createElement("h1");// le estoy diciendo que debe crear un parrafo
+    const nuevoParrafo = document.createElement("h2");
+    const nuevoDivContent = document.createElement("div")
+    //le asignamos las clases a los div que hemos creado  
+    nuevoDiv.className = "card";
     nuevoDivImg.className = "imgBx";
     nuevoDivContent.className = "content";
-
-
+    //le asignamos el contenido
     nuevaImagen.src = resultadoPersonajes[i].image;
     nuevoNombre.innerHTML = resultadoPersonajes[i].name;
     nuevoParrafo.innerHTML = "Species: " + resultadoPersonajes[i].species + " <br>Gender: " + resultadoPersonajes[i].gender + "<br>Status: " + resultadoPersonajes[i].status + "<br> Origin: " + resultadoPersonajes[i].origin.name + "<br> Type: " + resultadoPersonajes[i].type;
-
-
-
+   //realizamos la estructura con el appendchild
     nuevoDivImg.appendChild(nuevaImagen);//sonre el nuevo elemento se va acrear  la imagen 
     nuevoDivContent.appendChild(nuevoNombre);
     nuevoDivContent.appendChild(nuevoParrafo);
     nuevoDiv.appendChild(nuevoDivImg);
     nuevoDiv.appendChild(nuevoDivContent);
     contenedorpersonajes.appendChild(nuevoDiv);//crear el div que contiene la imagen y que contiene el nombre copiando el formato de card
-
     // esto es para mostrar los episodios en un popup
-    nuevoDiv.id = todosLosPersonajes[i].id
-
+    nuevoDiv.id = todosLosPersonajes[i].name;
     nuevoDiv.addEventListener('click', showEpisodes);
   }
-
-
 }
 //esto es para mostrar los episodios en un popup
-
-
-
 function showEpisodes(event) {
 
   document.getElementById("modalPopup").style.display = "block";
   let id = event.currentTarget.id;
-  console.log(id);
-  let infoObjectById = todosLosPersonajes.find(elemento => elemento.id == id)
-  console.log(infoObjectById);
+ console.log(id); // me muestra el id
+  let infoObjectById = todosLosPersonajes.find(elemento => elemento.name == id)
   let contenedorpersonajes = document.getElementById("modalPopup");// creo las variables la ubicacion a remplazar
-
   let showBirthPlace = infoObjectById.origin.name;
   let newBirthPlace = document.createElement("h1");
   newBirthPlace.innerHTML = showBirthPlace;
   contenedorpersonajes.appendChild(newBirthPlace);
 }
 
-
+//ejecutamos la busqueda con el keyup y la funcion filter name que esta en data.js
 let busquedaInput = document.getElementById("filtrarBusqueda");
 busquedaInput.addEventListener("keyup", ejecutarBusqueda);
 
 function ejecutarBusqueda() {
   let parametroBusqueda = busquedaInput.value;
-
   let resultados = filterItems(todosLosPersonajes, parametroBusqueda);
-
   showCards(resultados);
 }
 
-
-
-document.getElementById("seccionDimensiones").addEventListener("click", listLocations);
-
+//ejecutamos el filtro por planetas
+let btnDimensions=document.getElementById("seccionDimensiones");
+btnDimensions.addEventListener("click", listLocations);
 
 function listLocations() {
+
   document.getElementById("personajes").style.display = "none";
   document.getElementById("filtrarBusqueda").style.display ="none";
 
   let planetas = [];
-  // creamos un array con todos los planetas
+  // creamos un array con todos los planetas aca hay un array creado por cada planeta encontrado dentro de los objetos
   for (let i = 0; i < todosLosPersonajes.length; i++) {
     planetas.push(todosLosPersonajes[i].location.name);
-  }
-  // console.log(planetas);
-  // let planetasUnicos =[...new Set(planetas)];
-  // console.log( planetasUnicos);
-    
+      }
+      console.log(planetas);
   let planetasDiferentes = [];
+  
   planetas.forEach(
     (item) => {
       if (planetasDiferentes.includes(item)) { //no hacer nada
       } else { planetasDiferentes.push(item) }
     })
   console.log(planetasDiferentes);
+  // console.log(planetasDiferentes[5]);
 
   let mostrarPlanetas = planetasDiferentes.map(function(locaciones){
-    return '<button>'+locaciones+'</button>'
+   const botonesPlanetas= `<button value=${locaciones}>`+locaciones+`</button>`;
+   return botonesPlanetas;
+    
   })
   document.getElementById("locations").innerHTML = mostrarPlanetas;
   
@@ -145,6 +132,7 @@ function listStatus() {
 document.getElementById("seccionPersonajes").addEventListener("click", listCharacters);
 function listCharacters() {
   let placecharacters = document.getElementById("personajes");
+ 
   placecharacters.style.display = "block";
   // document.getElementById("personajes").style.display = "block";
 }
@@ -154,13 +142,7 @@ const mostrarPlanetas = document.getElementsByClassName("classLocations");
 for (let i = 0; i < mostrarPlanetas.length; i++) {
  
   mostrarPlanetas[i].addEventListener("click", () => {
-    // Buscamos el div que contiene la vista de los chacarcters
-    mostrarPlanetas.id =  mostrarPlanetas[i].location;
-    console.log("dame click");
-    // console.log(mostrarPlanetas[i]);
-    const showDimensiones = document.getElementById("personajes");
-    showDimensiones.style.display = "block";
-  
-  });
-  // console.log(mostrarPlanetas[i]);
+    console.log("se dio un click sobre el boton");
+    });
+  console.log(mostrarPlanetas[i]);
 }
